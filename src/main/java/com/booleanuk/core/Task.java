2ef2755e15d6f5a1470b509d46b2c720077d345b
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-08-15 09:39:15 +0200

    Test adding to ArrayList() tasks fails

---
+++ src/main/java/com/booleanuk/core/Task.java
@@ -0,0 +1,12 @@
+package com.booleanuk.core;
+
+public class Task {
+
+    String name;
+    Boolean completed;
+
+    Task(String name){
+        this.name = name;
+        this.completed = false;
+    }
+}
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,13 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    TodoList todoList = new TodoList();
+
+    @Test
+    public void testAdd(){
+        Task taskOne = new Task("taskOne");
+        Assertions.assertEquals("taskOne added.", todoList.add(taskOne));
+        //Assertions.assertTrue(todoList.tasks.contains("taskOne"));
+    }
 }
