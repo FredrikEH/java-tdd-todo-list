ad1994baa9e0e37271f1f797b01c2bd3c36f40d2
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-08-15 11:12:22 +0200

    Test if seeIncompleteTasks() works fail

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -53,4 +53,17 @@ class TodoListTest {
         todoList.changeStatus(taskThree);
         Assertions.assertEquals("taskOne, taskThree", todoList.seeCompletedTasks());
     }
+
+    @Test
+    public void testTasksIncomplete(){
+        Task taskOne = new Task("taskOne");
+        Task taskTwo = new Task("taskTwo");
+        Task taskThree = new Task("taskThree");
+        todoList.add(taskOne);
+        todoList.add(taskTwo);
+        todoList.add(taskThree);
+        todoList.changeStatus(taskOne);
+        todoList.changeStatus(taskThree);
+        Assertions.assertEquals("taskTwo, ", todoList.seeIncompleteTasks());
+    }
 }
